49664d794fa4878ddfbb74f219f647c71d0eff37
Priyal Tripathi <136731031+priyalT@users.noreply.github.com>
2025-01-15 12:11:42 +0530

    Update TCGABiolinks_BRCA_clinical.R

--- Preprocess/TCGABiolinks_BRCA_clinical.R
+++ Preprocess/TCGABiolinks_BRCA_clinical.R
@@ -1,21 +1,54 @@
-if (!requireNamespace("BiocManager", quietly = TRUE)) {
-  install.packages("BiocManager")
-}
+install.packages("janitor")
 
-BiocManager::install("TCGAbiolinks")
-
-#Load necessary libraries
 library(TCGAbiolinks)
-library(dplyr)
-library(magrittr)
+library(GEOquery)
+library(tidyverse)
+library(janitor)
 
-#Query clinical data for Breast Cancer
+cancerTypesamples <- getGEOSuppFiles("GSE62944", makeDirectory = F, baseDir = getwd(), filter_regex = "GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz")
+CancerType <- rownames(cancerTypesamples) %>%
+  read_tsv(col_names = F)
+colnames(CancerType) <- c("Sample_ID", "cancer_type")
+CancerType <- CancerType %>%
+  dplyr::filter(cancer_type == "BRCA")
+
+normalTypesamples <- getGEOSuppFiles("GSE62944", makeDirectory = F, baseDir = getwd(), filter_regex = "GSE62944_06_01_15_TCGA_24_Normal_CancerType_Samples.txt.gz")
+NormalType <- rownames(normalTypesamples) %>%
+  read_tsv(col_names = F)
+colnames(NormalType) <- c("Sample_ID", "cancer_type")
+NormalType <- NormalType %>%
+  dplyr::filter(cancer_type == "BRCA")%>%
+  mutate(bcr_patient_barcode = substr(Sample_ID, 1, 12))
+
+na_strings <- c("NA", "[Unknown]", "[Not available]", "[Not Evaluated]", "[Not Applicable]")
+Sys.setenv("VROOM_CONNECTION_SIZE" = 500000)
+clinicalVariables <- getGEOSuppFiles("GSE62944", makeDirectory = F, baseDir = getwd(), filter_regex = "GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz")
+Clinical_Variables <- rownames(clinicalVariables) %>%
+  read_tsv(col_names = F, na = na_strings) %>%
+  dplyr::select(-(X2:X3))
+
+Transposed_df <- as_tibble(t(Clinical_Variables), stringsAsFactors = F)
+Transposed_df[1,1] <- "Sample_ID"
+Transposed_df <- row_to_names(Transposed_df, 1, remove_row = TRUE, remove_rows_above = TRUE)
+
+Merged_tumor_df <- Transposed_df %>%
+  inner_join(CancerType, by = "Sample_ID") %>%
+  dplyr::select(-cancer_type)
+
+
+Merged_normal_df <- Transposed_df %>%
+  inner_join(NormalType, by ="bcr_patient_barcode") %>%
+  rename(Sample_ID = Sample_ID.x) %>%
+  dplyr::select(-c(Sample_ID.y, cancer_type))
+
+
 query <- GDCquery(
   project = "TCGA-BRCA", 
   data.category = "Clinical",
   data.type = "Clinical Supplement", 
   data.format = "BCR Biotab"
 )
+
 
 #Download the data
 GDCdownload(query)
@@ -37,10 +70,6 @@ library(DT)
 clinical.BCRtab.all$clinical_patient_brca  %>% 
   head() %>% 
   DT::datatable(options = list(scrollX = TRUE, keys = TRUE))
-
-#store the clinical information in a new variable and write it into a csv
-data = clinical.BCRtab.all$clinical_patient_brca
-write.csv(data,"/Users/priyaltripathi/Documents/iit/TCGA-BRCA_clinical.csv", row.names = FALSE)
 
 #query molecular subtypes and store them under mol_subtypes and write it into a csv
 mol_subtypes <- TCGAquery_subtype(tumor = "brca")
