bb51e053bc41eda2f002d03a4610616598b8cebe
Priyal Tripathi <136731031+priyalT@users.noreply.github.com>
2025-05-03 20:27:02 +0530

    Update Intersection.R

--- Preprocess/Intersection.R
+++ Preprocess/Intersection.R
@@ -1,14 +1,14 @@
 #Intersection of gene_dataset and expression_data
-a <- gene_dataset
-b <- expression_data
+a <- read.csv("/Users/priyaltripathi/Multicluster Analysis/Preprocess/TCGA/TCGA-BRCA_clinical.csv")
+b <- read.csv("/Users/priyaltripathi/Multicluster Analysis/Preprocess/Indian/BRCA_Indian_Gene_data.csv")
 tcga <- colnames(a)
 indian <- colnames(b)
 class(tcga)
 intersection_col <- intersect(tcga,indian)
 a <- a[, intersection_col]
 b <- b[, intersection_col]
-write.csv(a,"/Users/priyaltripathi/Documents/iit/TCGA-BRCA_intersect.csv", row.names = TRUE)
-write.csv(b,"/Users/priyaltripathi/Documents/iit/Indian_intersect.csv", row.names = TRUE)
+write.csv(a,"/Users/priyaltripathi/Multicluster Analysis/Preprocess/Intersection/TCGA-BRCA_intersect.csv", row.names = TRUE)
+write.csv(b,"/Users/priyaltripathi/Multicluster Analysis/Preprocess/Intersection/Indian_intersect.csv", row.names = TRUE)
 
 TC <- read.csv("TCGA-BRCA_intersect.csv")
 save(TC, file = "TCGA-BRCA_intersect.RData")
